c70fd06a0ac2e6ce0e170e7fe71531e073972122
Bruno Barroso Rodrigues <bruno.rodrigues@geq.com.br>
2016-11-10 15:49:26 -0300

    Project boilerplate plugin wordpress

---
+++ includes/class-sell-with-pagseguro-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://facebook.com/brunobarrosor
+ * @since      1.0.0
+ *
+ * @package    Sell_With_Pagseguro
+ * @subpackage Sell_With_Pagseguro/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sell_With_Pagseguro
+ * @subpackage Sell_With_Pagseguro/includes
+ * @author     Bruno Barroso <brunobinfo@gmail.com>
+ */
+class Sell_With_Pagseguro_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
